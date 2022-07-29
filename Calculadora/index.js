91b43bfe78cc88c28d5d396fc6d1b792fb0d7b83
carianha86 <110254014+carianha86@users.noreply.github.com>
2022-07-29 14:47:06 -0300

    Add files via upload

---
+++ Calculadora/index.js
@@ -0,0 +1,123 @@
+/*var n = 7
+
+for (let i = 0; i <= 10; i++) {
+   console.log( `${i} X ${n} = ${i*n}`);
+}
+
+*/
+
+/*function soma (x1,x2){
+
+return x1 + x2;
+
+}
+
+let resultado = soma (1,2);
+
+console.log(resultado);
+*/
+
+/*function calc (x,y,operator){
+
+return eval ( ` ${x} ${operator} ${y}`);
+
+}
+
+let resultado = calc (5,2,"*");
+
+console.log(resultado);
+
+*/
+/*
+let calc = (x,y,operator) => {
+
+    return eval ( ` ${x} ${operator} ${y}`);
+    
+    }
+    
+    let resultado = calc (5,3,"*");
+    
+    console.log(resultado);  */
+
+   /* window.addEventListener('focus', event => {
+
+      console.log('focus');
+
+
+    });
+
+    document.addEventListener('click', event => {
+
+        console.log('clickkk');
+    });
+    */
+
+   /* let data = new Date();
+
+    console.log(data.getFullYear(), data.getMonth(), data.getDate());
+
+    console.log(data.toLocaleDateString("pt-BR"));*/
+
+    //let carros = ["uno", "palio 98", "amarok", "meriva"];
+
+//console.log(carros);
+
+//console.log(carros.length);
+
+//console.log(carros[1]);
+
+//carros.forEach(function(value, index){
+
+//console.log(index, value);
+
+//})
+/*
+let celular = function() {  //metodo antigo do java script de escrever classe
+
+    this.cor = "preto"   // Atributo
+
+    this.ligar = function(){  // Metodo
+
+        console.log("uma ligação");
+        return "ligando";
+
+    }
+}
+*/
+/*
+class celular {  //metodo novo do java script de escrever classe
+
+    constructor(){
+
+    this.cor = "preto";   // Atributo
+
+
+    }
+
+     ligar(){  // Metodo
+
+        console.log("uma ligação");
+        return "ligando";
+
+    }
+
+
+    
+
+}
+
+/*let objeto = new celular();
+
+    console.log(objeto);
+*/
+    /*
+let objeto = new celular();
+
+    console.log(objeto);
+
+    console.log(objeto.ligar);
+    
+    
+    */
+
+
